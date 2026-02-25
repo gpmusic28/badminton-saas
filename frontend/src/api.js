19141035938a562d13ea073871848558a4e9e485
connectsmfs <connect@shreemfs.com>
2026-02-25 16:13:06 +0530

    Fix API baseURL for production

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
-const API = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}/api`
-});
+const baseURL =
+  process.env.REACT_APP_API_URL
+    ? `${process.env.REACT_APP_API_URL}/api`
+    : "http://localhost:3001/api";
 
-API.interceptors.request.use(config => {
-  const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
+console.log("API BASE URL:", baseURL);
+
+const API = axios.create({
+  baseURL,
+  withCredentials: true
 });
 
 export default API;
